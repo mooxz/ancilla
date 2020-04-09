48641128250a9d259005b4520c407fc38820ff29
hide <duxinzehide@gmail.com>
2020-04-09 22:20:49 +0800

    project commit

--- lib/pages/main/bottom_bar_items.dart
+++ lib/pages/main/bottom_bar_items.dart
@@ -4,7 +4,7 @@ class MainBottomBarItem extends BottomNavigationBarItem {
 	MainBottomBarItem(String iconName, String title)
 	: super(
 		title: Text(title),
-		icon: Image.asset('assets/images/tabbar/$iconName.png', width: 32),
-		activeIcon: Image.asset('assets/images/tabbar/${iconName}_active.png', width: 32),
+		icon: Image.asset('assets/images/tabbar/$iconName.png', width: 32, gaplessPlayback: true),
+		activeIcon: Image.asset('assets/images/tabbar/${iconName}_active.png', width: 32, gaplessPlayback: true),
 	);
 }
